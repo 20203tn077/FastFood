47b89acd01738d05cdf2c89b7d3c78e6bfb44487
Rebecca C <20203tn077@utez.edu.mx>
2023-01-27 16:51:55 -0600

    Avance DAOs

--- src/main/java/mx/edu/utez/fastfood/fastfood/model/category/CategoryDao.java
+++ src/main/java/mx/edu/utez/fastfood/fastfood/model/category/CategoryDao.java
@@ -1,4 +1,25 @@
 package mx.edu.utez.fastfood.fastfood.model.category;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryDao {
+    private Connection con;
+    private PreparedStatement pstm;
+    private ResultSet rs;
+
+    public List<Category> getAll() {
+        List<Category> categories = new ArrayList<>();
+        // TODO: 27/01/2023
+        return categories;
+    }
+
+    public Category findByDishId(long id) {
+        Category category = null;
+        // TODO: 27/01/2023
+        return category;
+    }
 }
--- src/main/java/mx/edu/utez/fastfood/fastfood/model/dish/DishDao.java
+++ src/main/java/mx/edu/utez/fastfood/fastfood/model/dish/DishDao.java
@@ -1,4 +1,48 @@
 package mx.edu.utez.fastfood.fastfood.model.dish;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DishDao {
+    private Connection con;
+    private PreparedStatement pstm;
+    private ResultSet rs;
+
+    public List<Dish> findAll() {
+        List<Dish> dishes = new ArrayList<>();
+        // TODO: 27/01/2023
+        return dishes;
+    }
+
+    public boolean existsById(long id) {
+        boolean exists = false;
+        // TODO: 27/01/2023
+        return exists;
+    }
+    public Dish findById(long id) {
+        Dish dish = null;
+        // TODO: 27/01/2023
+        return dish;
+    }
+
+    public boolean create(Dish dish) {
+        boolean success = false;
+        // TODO: 27/01/2023
+        return success;
+    }
+
+    public boolean update(Dish dish) {
+        boolean success = false;
+        // TODO: 27/01/2023
+        return success;
+    }
+
+    public boolean delete(long id) {
+        boolean success = false;
+        // TODO: 27/01/2023
+        return success;
+    }
 }
