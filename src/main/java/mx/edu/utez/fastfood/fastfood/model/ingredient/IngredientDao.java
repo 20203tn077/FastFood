5878bc6fcb0678d5fc5f087a5c23181a171a625a
Rebecca C <20203tn077@utez.edu.mx>
2023-01-27 16:51:55 -0600

    Avance DAOs

--- src/main/java/mx/edu/utez/fastfood/fastfood/model/ingredient/IngredientDao.java
+++ src/main/java/mx/edu/utez/fastfood/fastfood/model/ingredient/IngredientDao.java
@@ -1,4 +1,25 @@
 package mx.edu.utez.fastfood.fastfood.model.ingredient;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class IngredientDao {
+    private Connection con;
+    private PreparedStatement pstm;
+    private ResultSet rs;
+
+    public List<Ingredient> findAll() {
+        List<Ingredient> ingredients = new ArrayList<>();
+        // TODO: 27/01/2023
+        return ingredients;
+    }
+
+    public List<Ingredient> findByDishId(long id) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        // TODO: 27/01/2023
+        return ingredients;
+    }
 }
--- src/main/java/mx/edu/utez/fastfood/fastfood/service/MySqlConnection.java
+++ src/main/java/mx/edu/utez/fastfood/fastfood/service/MySqlConnection.java
@@ -1,10 +1,55 @@
 package mx.edu.utez.fastfood.fastfood.service;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 public class MySqlConnection {
-    public static Connection getConnection() {
-        ResourceBundle bundle = ResourceBundle.getBundle("/properties");
+    private static String host;
+    private static String port;
+    private static String database;
+    private static String useSSL;
+    private static String timezone;
+    private static String user;
+    private static String password;
+
+    private static ResourceBundle connectionProperties;
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch(ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        if (connectionProperties == null) {
+            connectionProperties = ResourceBundle.getBundle("connection");
+            host = connectionProperties.getString("host");
+            port = connectionProperties.getString("port");
+            database = connectionProperties.getString("database");
+            useSSL = connectionProperties.getString("useSSL");
+            timezone = connectionProperties.getString("timezone");
+            user = connectionProperties.getString("user");
+            password = connectionProperties.getString("password");
+        }
+
+        return DriverManager.getConnection(String.format("jdbc:mysql://%s:%s/%s?useSSL=%s&serverTimezone=%s", host, port, database, useSSL, timezone), user, password);
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement pstm, ResultSet rs) {
+        try {
+            if (rs != null) rs.close();
+            if (pstm != null) pstm.close();
+            if (con != null) con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            System.out.println(MySqlConnection.getConnection() == null ? "Conexión fallida :(" : "Conexión exitosa :)");
+        } catch (SQLException e) {
+            System.out.println("Conexión fallida");
+            e.printStackTrace();
+        }
     }
 }
