5109bc07a5b025fa41350696c1eda363d6c21ed8
Ricardo C <20203tn077@utez.edu.mx>
2023-01-26 20:37:22 -0600

    avance menor

--- src/main/java/mx/edu/utez/fastfood/fastfood/service/MySqlConnection.java
+++ src/main/java/mx/edu/utez/fastfood/fastfood/service/MySqlConnection.java
@@ -1,4 +1,10 @@
 package mx.edu.utez.fastfood.fastfood.service;
 
+import java.sql.Connection;
+import java.util.ResourceBundle;
+
 public class MySqlConnection {
+    public static Connection getConnection() {
+        ResourceBundle bundle = ResourceBundle.getBundle("/properties");
+    }
 }
