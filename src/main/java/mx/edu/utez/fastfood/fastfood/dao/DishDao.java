07c48b7572c8df656121c0fd2b684f85da390fcf
Rebecca C <20203tn077@utez.edu.mx>
2023-01-28 12:03:38 -0600

    declaración de métodos para manejo de relacion many to many con ingredient

--- src/main/java/mx/edu/utez/fastfood/fastfood/dao/DishDao.java
+++ src/main/java/mx/edu/utez/fastfood/fastfood/dao/DishDao.java
@@ -1,6 +1,7 @@
 package mx.edu.utez.fastfood.fastfood.dao;
 
 import mx.edu.utez.fastfood.fastfood.model.Dish;
+import mx.edu.utez.fastfood.fastfood.model.Ingredient;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -45,6 +46,42 @@ public class DishDao {
     public boolean delete(long id) {
         boolean success = false;
         // TODO: 27/01/2023
+        return success;
+    }
+
+    public boolean attachIngredient(long dishId, Ingredient ingredient) {
+        boolean success = false;
+        // TODO: 28/01/2023
+        return success;
+    }
+
+    public boolean attachIngredients(long dishId, List<Ingredient> ingredients) {
+        boolean success = false;
+        // TODO: 28/01/2023
+        return success;
+    }
+
+    public boolean detachIngredient(long dishId, Ingredient ingredient) {
+        boolean success = false;
+        // TODO: 28/01/2023
+        return success;
+    }
+
+    public boolean detachIngredients(long dishId, List<Ingredient> ingredients) {
+        boolean success = false;
+        // TODO: 28/01/2023
+        return success;
+    }
+
+    public boolean detachAllIngredients(long dishId) {
+        boolean success = false;
+        // TODO: 28/01/2023
+        return success;
+    }
+
+    public boolean syncIngredients(long dishId, List<Ingredient> ingredient) {
+        boolean success = false;
+        // TODO: 28/01/2023
         return success;
     }
 }
