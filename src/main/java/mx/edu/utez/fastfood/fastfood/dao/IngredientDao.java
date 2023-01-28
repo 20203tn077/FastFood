3f96e6b1d8fa96b96e6f972d40499d5d002d11b0
Rebecca C <20203tn077@utez.edu.mx>
2023-01-28 13:28:33 -0600

    Elaboración de DAO de ingredientes

--- src/main/java/mx/edu/utez/fastfood/fastfood/dao/IngredientDao.java
+++ src/main/java/mx/edu/utez/fastfood/fastfood/dao/IngredientDao.java
@@ -1,10 +1,13 @@
 package mx.edu.utez.fastfood.fastfood.dao;
 
+import mx.edu.utez.fastfood.fastfood.exception.DaoException;
 import mx.edu.utez.fastfood.fastfood.model.Ingredient;
+import mx.edu.utez.fastfood.fastfood.service.MySqlConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +16,21 @@ public class IngredientDao {
     private PreparedStatement pstm;
     private ResultSet rs;
 
-    public List<Ingredient> findAll() {
+    public List<Ingredient> findAll() throws DaoException{
         List<Ingredient> ingredients = new ArrayList<>();
-        // TODO: 27/01/2023
-        return ingredients;
-    }
-
-    public List<Ingredient> findByDishId(long id) {
-        List<Ingredient> ingredients = new ArrayList<>();
-        // TODO: 27/01/2023
+        try {
+            con = MySqlConnection.getConnection();
+            pstm = con.prepareStatement("SELECT i.id, i.name FROM ingredient i ORDER BY i.name;");
+            rs = pstm.executeQuery();
+            while (rs.next()) ingredients.add(new Ingredient(
+                    rs.getLong("i.id"),
+                    rs.getString("i.name")
+            ));
+        } catch (SQLException e) {
+            throw new DaoException("Error en IngredientDao:findAll", e);
+        } finally {
+            MySqlConnection.closeConnection(con, pstm, rs);
+        }
         return ingredients;
     }
 }
