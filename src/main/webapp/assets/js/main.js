6fcceb5b67b86a15158edd638fa52a3fa7e5a60c
Rebecca C <20203tn077@utez.edu.mx>
2023-01-31 14:56:22 -0600

    avance

--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -1,17 +1,12 @@
 const confirmation = {
-    modalElement: document.getElementById('confirmation_modal'),
     modal: new bootstrap.Modal(document.getElementById('confirmation_modal'), {backdrop: 'static', keyboard: false}),
     txtTitle: document.getElementById('confirmation_txtTitle'),
     txtText: document.getElementById('confirmation_txtText'),
     btnConfirm: document.getElementById('confirmation_btnConfirm'),
     btnCancel: document.getElementById('confirmation_btnCancel'),
     show: function (title, text, form, modal) {
-        this.btnConfirm.onclick = () => {
-            this.modal.hide()
-            form.submit()
-            if (modal) modal.show()
-        }
-        this.btnCancel.onclick = () => {
+        this.btnConfirm.setAttribute('form', form.getAttribute('id'))
+        this.btnConfirm.onclick = this.btnCancel.onclick = () => {
             this.modal.hide()
             if (modal) modal.show()
         }
@@ -19,6 +14,24 @@ const confirmation = {
         this.txtText.innerText = text
         this.modal.show()
     },
+}
+
+const notification = {
+    toast: new bootstrap.Toast(document.getElementById('notification_toast')),
+    elToast: document.getElementById('notification_toast'),
+    elIcon: document.getElementById('notification_elIcon'),
+    txtMessage: document.getElementById('notification_txtMessage'),
+    show: function (message, success) {
+        this.txtMessage.innerText = message
+        if (success) {
+            this.elIcon.classList.replace('fa-times', 'fa-check')
+            this.elToast.classList.replace('text-bg-danger', 'text-bg-success')
+        } else {
+            this.elIcon.classList.replace('fa-check', 'fa-times')
+            this.elToast.classList.replace('text-bg-success', 'text-bg-danger')
+        }
+        this.toast.show()
+    }
 }
 
 const dishCreation = {
@@ -30,10 +43,13 @@ const dishCreation = {
         this.modal.show()
     },
     confirm: function () {
-        confirmation.show('Confirmar registro', '¿Estás seguro de registrar un platillo con los datos ingresados?', this.form, this.modal)
+        if (this.form.reportValidity()) confirmation.show('Confirmar registro', '¿Estás seguro de registrar un platillo con los datos ingresados?', this.form, this.modal)
+        else notification.show('Hay campos incompletos o con errores')
     },
 }
+
 const dishUpdate = {}
+
 const dishDeletion = {
     form: document.getElementById('dishDeletion_form'),
     inpId: document.getElementById('dishDeletion_inpId'),
