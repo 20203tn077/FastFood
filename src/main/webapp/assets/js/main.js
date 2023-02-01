60c6b60e9a7dd2ee272ac6b8c161c0648ce11ba9
Rebecca C <20203tn077@utez.edu.mx>
2023-02-01 14:57:10 -0600

    Avance final

--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -5,7 +5,7 @@ const confirmation = {
     btnConfirm: document.getElementById('confirmation_btnConfirm'),
     btnCancel: document.getElementById('confirmation_btnCancel'),
     show: function (title, text, form, modal) {
-        modal.hide()
+        if (modal) modal.hide()
         this.btnConfirm.setAttribute('form', form.getAttribute('id'))
         this.btnConfirm.onclick = this.btnCancel.onclick = () => {
             this.modal.hide()
@@ -44,13 +44,46 @@ const dishCreation = {
         this.modal.show()
     },
     confirm: function () {
-        console.log('123', this.form.reportValidity())
         if (this.form.reportValidity()) confirmation.show('Confirmar registro', '¿Estás seguro de registrar un platillo con los datos ingresados?', this.form, this.modal)
         else notification.show('Hay campos incompletos o con errores')
     },
 }
 
-const dishUpdate = {}
+const dishUpdate = {
+    modal: new bootstrap.Modal(document.getElementById('dishUpdate_modal'), {backdrop: 'static', keyboard: false}),
+    form: document.getElementById('dishUpdate_form'),
+    inpName: document.getElementById('dishUpdate_inpName'),
+    inpPrice: document.getElementById('dishUpdate_inpPrice'),
+    inpDescription: document.getElementById('dishUpdate_inpDescription'),
+    inpCategory: document.getElementById('dishUpdate_inpCategory'),
+    inpId: document.getElementById('dishUpdate_inpId'),
+    btnUpdate: document.getElementById('dishUpdate_btnUpdate'),
+    show: function (id) {
+        this.form.reset()
+        fetch(context + '/Platillos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `&action=find&id=${id}`
+        }).then(res => res.json()).then(({dish}) => {
+            this.inpId.value = id
+            this.inpName.value = dish.name
+            if (dish.description) this.inpDescription.value = dish.description
+            this.inpPrice.value = dish.price
+            this.inpCategory.value = dish.category.id
+            for (ingredient of dish.ingredients) document.getElementById(`dishUpdate_ingredient${ingredient.id}`).checked = true
+            this.modal.show()
+        }).catch(res => {
+            notification.show(res.data.message)
+        })
+
+    },
+    confirm: function () {
+        if (this.form.reportValidity()) confirmation.show('Confirmar actualización', '¿Estás seguro de actualizar la información del platillo?', this.form, this.modal)
+        else notification.show('Hay campos incompletos o con errores')
+    },
+}
 
 const dishDeletion = {
     form: document.getElementById('dishDeletion_form'),
